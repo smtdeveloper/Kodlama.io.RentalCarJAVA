73fc81550079f6d22d9cd1abdf6381d3143d7ae8
Samet Akca <developer2@cevizsoft.com>
2022-11-03 21:13:02 +0300

    5. Gün

---
+++ src/main/java/kodlama/io/rentACar/business/responses/GetAllBrandResponse.java
@@ -0,0 +1,15 @@
+package kodlama.io.rentACar.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetAllBrandResponse {
+
+	private int id;
+	private String name;
+	
+}
--- src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.responses.GetAllBrandResponse;
 import kodlama.io.rentACar.entities.concretes.Brand;
 
 @RestController 
@@ -23,12 +27,17 @@ public class BrandsController {
 	}
 	
 	@GetMapping("/getall")
-	public List<Brand> getAll()
+	public List<GetAllBrandResponse> getAll()
 	{
 		return brandService.getAll();
 		
 	}
 	
+	@PostMapping("/add")
+	public void Add(@RequestBody() CreateBrandRequest request ) 
+	{
+		 this.brandService.add(request);
+	}
 	
 	
 }
